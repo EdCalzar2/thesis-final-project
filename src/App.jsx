7fbb29abc0e57c96a72c36bdb541bed776b59563
EdCalzar2 <calzar.edrick122503@gmail.com>
2025-05-30 12:31:03 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import './components/pages/Login.css'; 
 import Login from './components/pages/Login'; 
 import MainPage from './MainPage'; 
-import { Home, Story, Safety_map } from './components/pages'; 
-import Admin from './components/pages/Admin';
+import { Story, Safety_map } from './components/pages'; 
+import ManageStories from './components/pages/ManageStories';
 
 function App() { 
   const [stories, setStories] = useState([]);
@@ -12,14 +12,12 @@ function App() {
     <Routes>
       {/* Route for the login page */}
       <Route path="/" element={<Login />} />
-      {/* Route for the home page, wrapped in MainPage layout */}
-      <Route path="/home" element={<MainPage><Home /></MainPage>} />
       {/* Route for the story page, wrapped in MainPage layout */}
       <Route path="/story" element={<MainPage><Story stories={stories} setStories={setStories} /></MainPage>} />
       {/* Route for the safety map page, wrapped in MainPage layout */}
       <Route path="/safety_map" element={<MainPage><Safety_map /></MainPage>} />
       {/* Route for the admin page, NO MainPage layout (no Navbar) */}
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/manageStories" element={<ManageStories />} />
     </Routes>
   );
 }
