015f392ac3a239c528f65d3946a1e3d69d579a17
EdCalzar2 <calzar.edrick122503@gmail.com>
2025-05-30 12:40:04 +0800

    Update StoryItem.jsx

--- src/components/StoryItem.jsx
+++ src/components/StoryItem.jsx
@@ -1,9 +1,18 @@
 import styles from './storyItem.module.css'
 
-export default function StoryItem({ item }) {
+export default function StoryItem({ item, onDelete }) {
+    const handleDelete = () => {
+        onDelete(item);
+    };
+
     return (
+        <>
         <div className={styles.item}>
             <div className={styles.itemName}>{item}</div>
+            <button className={styles.deleteButton} onClick={handleDelete}>
+                Delete
+            </button>
         </div>
+        </>
     )
 }
