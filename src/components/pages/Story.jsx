0476908c460e3f111f55fcaa2d4f6f3fb0b444a4
EdCalzar2 <calzar.edrick122503@gmail.com>
2025-05-30 14:57:47 +0800

    Update Story.jsx

--- src/components/pages/Story.jsx
+++ src/components/pages/Story.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from '../Header'
 import Form from '../Form'
 import StoryList from '../StoryList'
 
-export const Story = () => {
-    const [stories, setStories] = useState([])
+export const Story = ({ stories, setStories }) => {
+    const handleDeleteStory = (storyToDelete) => {
+        setStories(stories.filter(story => story !== storyToDelete));
+    };
+
     return (
         <div>
             <Header/>
             <Form stories={stories} setStories={setStories} />
-            <StoryList stories={stories} />
+            <StoryList stories={stories} onDeleteStory={handleDeleteStory} />
         </div>
     )
 }
