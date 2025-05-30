1434d37f34d6bd54fc2441f7a67277497c1815b5
EdCalzar2 <calzar.edrick122503@gmail.com>
2025-05-30 14:55:37 +0800

    Update StoryList.jsx

--- src/components/StoryList.jsx
+++ src/components/StoryList.jsx
@@ -1,11 +1,11 @@
 import StoryItem from './StoryItem'
 import styles from './storyList.module.css'
 
-export default function StoryList({stories}) {
+export default function StoryList({stories, onDeleteStory}) {
     return (
         <div className={styles.list}>
             {stories.map((item) => (
-                <StoryItem key={item} item={item}/>
+                <StoryItem key={item} item={item} onDelete={onDeleteStory}/>
             ))}
         </div>
     )
