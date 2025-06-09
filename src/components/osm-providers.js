24b6d2752210502a386df70815f10fd6e78b3f5a
EdCalzar2 <calzar.edrick122503@gmail.com>
2025-06-09 19:09:16 +0800

    Add files via upload

---
+++ src/components/osm-providers.js
@@ -0,0 +1,8 @@
+const osm = {
+    mapTiler: {
+        url: "https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=ZrndhvZv9TMB9oOYC1Yn",
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }
+}
+
+export default osm
